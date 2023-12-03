6b323870c5160abc2f68b96bc4f358d92aa9f06b
SyedZeeshanAijaz <syedzeeshanaijaz@gmail.com>
2023-12-03 20:49:21 +0500

    Added a WebApp

---
+++ ZeeshanWeb.App/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ZeeshanWeb.App.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
